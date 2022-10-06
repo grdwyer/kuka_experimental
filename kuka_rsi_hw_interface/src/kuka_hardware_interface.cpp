0aba893afa057b067ce4a484054e6a2674138625
George Dwyer <george.dwyer@ucl.ac.uk>
2022-10-06 14:01:01 +0100

    plugin for hardware not being found

--- kuka_rsi_hw_interface/src/kuka_hardware_interface.cpp
+++ kuka_rsi_hw_interface/src/kuka_hardware_interface.cpp
@@ -280,6 +280,7 @@ hardware_interface::return_type KukaHardwareInterface::write(const rclcpp::Time 
   return hardware_interface::return_type::OK;
 }
 
+} // namespace kuka_rsi_hw_interface
 
-
-} // namespace kuka_rsi_hardware_interface
+#include "pluginlib/class_list_macros.hpp"  // NOLINT
+PLUGINLIB_EXPORT_CLASS(kuka_rsi_hw_interface::KukaHardwareInterface, hardware_interface::SystemInterface)
